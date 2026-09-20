441a536540cb42f327680b21d0ebc43792fb81e1
Jon <jkissil@ufl.edu>
2026-09-20 17:34:33 -0400

    logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Leaf } from 'lucide-react'
 
 export function Logo({ light = false, to = '/' }) {
   return (
     <Link to={to} className={`logo ${light ? 'light' : ''}`}>
       <span>fullr</span>
-      <Leaf size={19} strokeWidth={2.4} />
+      <img src="/logo.png" alt="" aria-hidden="true" />
     </Link>
   )
 }
